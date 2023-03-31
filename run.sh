451c4225f571d3f9f626f4a3c7f68f2622082499
hezheyu <rinchannow@bupt.edu.cn>
2023-03-31 13:45:44 +0800

    Update data source.

--- run.sh
+++ run.sh
@@ -7,7 +7,12 @@ QUERY_DIR="queries"
 
 for i in {1..99}
 do
-    sql="query_$i.sql"
+    if [ $i -lt 10 ]
+    then
+        sql="0$i.sql"
+    else
+        sql="$i.sql"
+    fi
 
     echo "Run $sql start."
     cat "$QUERY_DIR"/"$sql" | $MYSQL_CLIENT_CONNECT
